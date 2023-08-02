d85e7a9d3fce483399f2fdef6f2bb79473e05963
nvdiw <nouraeinavid@gmail.com>
2023-08-02 16:41:48 +0430

    make_id added

--- make_contacts.py
+++ make_contacts.py
@@ -1,1 +1,19 @@
-print('hi')
+import names
+import random
+import csv
+import json
+
+n = 10
+# returns 'n' numbers id
+def make_id(n) :
+    lst_id = []
+    for i in range(n) :
+        id = random.randint(10000 , 20000)
+        if id not in lst_id :
+            lst_id.append(id)
+    return lst_id
+
+def main() :
+    print(make_id(n))
+
+main()
