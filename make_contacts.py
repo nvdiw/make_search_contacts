3dc39314413f629da98e38fc7d7b35fa04219489
nvdiw <nouraeinavid@gmail.com>
2023-08-02 17:55:36 +0430

    make_random_contacts added

--- make_contacts.py
+++ make_contacts.py
@@ -6,7 +6,7 @@ import json
 n = 10
 
 # returns 'n' numbers id
-def make_id(n) :
+def random_id(n) :
     lst_id = []
     for i in range(n) :
         id = random.randint(10000 , 20000)
@@ -15,7 +15,7 @@ def make_id(n) :
     return lst_id
 
 # return 'n' numbers first name
-def make_first_name(n) :
+def random_first_name(n) :
     lst_first_name = []
     for i in range(n):
         first_name = names.get_first_name()
@@ -23,17 +23,26 @@ def make_first_name(n) :
     return lst_first_name
 
 # return 'n' numbers last name
-def make_last_name(n) :
+def random_last_name(n) :
     lst_last_name = []
     for i in range(n):
         last_name = names.get_last_name()
         lst_last_name.append(last_name)
     return lst_last_name
 
+# returns merge {'id' 'first_name' 'last_name' 'phone_number'}
+def make_random_list(id , f_name , l_name) :
+    d_c = {}
+    d_contacts = []
+    for i in range(n) :
+        d_c = {'id' : id[i] , 'f_name' : f_name[i] , 'l_name' : l_name[i]}
+        d_contacts.append(d_c)
+    return d_contacts
 
 def main() :
-    print(make_id(n))
-    print(make_first_name(n))
-    print(make_last_name(n))
-    
+    id = random_id(n)
+    f_name = random_first_name(n)
+    l_name = random_last_name(n)
+    make_random_list(id , f_name , l_name)
+
 main()
