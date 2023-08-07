047fe0bb685871696fa4d9226108e3fc44ea1387
nvdiw <nouraeinavid@gmail.com>
2023-08-07 13:47:49 +0430

    search_contacts modified to modullar and debugging

--- search_contacts.py
+++ search_contacts.py
@@ -1,61 +1,73 @@
 import csv
 
 file = 'contacts_sorted_fname.csv'
-
 dataFrame = csv.reader(open(file , 'r'))
-
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-q = input('searching for : ').lower()
+input_q = input('searching for : ').lower()
 
-for i in q :
-    if i in numbers :
-        search_for = 'number'
-    else :
-        search_for = 'letter'
-        break
+# if number return number if letter return letter
+def number_letter_input(input_q) :
+    for i in input_q :
+        if i in numbers :
+            search_for = 'number'
+        else :
+            search_for = 'letter'
+            break
+    return search_for
 
-lst_answers = []
-for lst in dataFrame :
-    if lst[1] == 'first_name' :
-        continue
-    lst_answer = lst
-    c = 0
-    x = 0
+# print lists
+def print_lst(lst) :
+    for i in lst :
+        print(i)
 
-    if search_for == 'number' :
-        lst = lst[3].lower()
-        for my_letter in q :
+# returns list of searched numbers
+def search_number(input_q) :
+    lst_answers = []
+    for lst in dataFrame :
+        if lst[1] == 'first_name' :
+            continue
+        lst_answer = lst
+        lst = lst[3]
+        c = 0
+        x = 0
+        for my_letter in input_q :
             if my_letter != lst[c] :
                 break
             elif my_letter == lst[c] :
                 x += 1
-                if x == len(q) :
+                if len(input_q) == x :
                     lst_answers.append(lst_answer)
             c += 1
-    
+    return lst_answers
 
-    elif search_for == 'letter' :
-        lst2 = lst[2].lower()
-        lst = lst[1].lower()
-        for my_letter in q :
-            if len(q) > len(lst) :
-                if len(q) > len(lst2) :
+# returns list of searched names
+def search_name(input_q) :
+    lst_answers = []
+    for lst in dataFrame :
+        if lst[1] == 'first_name' :
+            continue
+        lst_answer = lst
+        lsts = [lst[1].lower() , lst[2].lower()]
+        for lst in lsts :
+            c = 0
+            x = 0
+            for my_letter in input_q :
+                if my_letter != lst[c] :
                     break
-            elif my_letter != lst[c] :
-                if my_letter != lst2[c] :
-                    break
-                else :
-                    if my_letter == lst2[c] :
-                        x += 1
-                        if x == len(q) :
-                            lst_answers.append(lst_answer)
-            elif my_letter == lst[c] :
-                x += 1
-                if x == len(q) :
-                    lst_answers.append(lst_answer)
+                elif my_letter == lst[c] :
+                    x += 1
+                    if len(input_q) == x :
+                        lst_answers.append(lst_answer)
+                if c + 1 < len(input_q) :
+                    c += 1
+    return lst_answers
 
-            
-            c += 1
 
-for i in lst_answers :
-    print(i)
+
+def main() :
+    if number_letter_input(input_q) == 'number' :
+        print_lst(search_number(input_q))
+    if number_letter_input(input_q) == 'letter' :
+        print_lst(search_name(input_q))
+
+main()
