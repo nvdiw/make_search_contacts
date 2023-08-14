7f85ee4c1b09e3bfda68345aab38a6771c430972
nvdiw <nouraeinavid@gmail.com>
2023-08-14 19:49:09 +0430

    little changes make better code

--- bubble_sort_str.py
+++ bubble_sort_str.py
@@ -4,6 +4,7 @@ import csv
 file = 'contacts.csv'
 dataFrame = csv.reader(open(file , 'r'))
 
+# returns sorted list 'bubble_sort'
 def bubble_sort(lst , x = 0) :
     for i in range(len(lst)) :
         for j in range(len(lst)-1-i) :
@@ -11,11 +12,13 @@ def bubble_sort(lst , x = 0) :
                 lst[j][x], lst[j+1][x] = lst[j+1][x], lst[j][x]
     return lst
 
+# runs into main function
 def main() :
     lst = []
     for i in dataFrame :
         lst.append(i)
     lst.remove(lst[0])
+
     for i in bubble_sort(lst , 1) :
         print(i)
 
