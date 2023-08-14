5c1c3b93222d284372311d2b9bca0fd4bb8c0327
nvdiw <nouraeinavid@gmail.com>
2023-08-14 19:47:19 +0430

    sorting by bubble_sort contacts.csv

---
+++ bubble_sort_str.py
@@ -0,0 +1,22 @@
+# lst = [['c','u'], ['d','t'], ['a','l'], ['b','m'], ['g','n'], ['e','o'], ['f','q'], ['j','p'], ['h','r'], ['i','s']]
+import csv
+
+file = 'contacts.csv'
+dataFrame = csv.reader(open(file , 'r'))
+
+def bubble_sort(lst , x = 0) :
+    for i in range(len(lst)) :
+        for j in range(len(lst)-1-i) :
+            if lst[j][x] > lst[j+1][x] :
+                lst[j][x], lst[j+1][x] = lst[j+1][x], lst[j][x]
+    return lst
+
+def main() :
+    lst = []
+    for i in dataFrame :
+        lst.append(i)
+    lst.remove(lst[0])
+    for i in bubble_sort(lst , 1) :
+        print(i)
+
+main()
